3ce77d30b978425da12bd87d5659111daa8b6b7e
kyohei <takanohashi.com@gmail.com>
2023-09-13 10:21:34 +0900

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by 鷹觜恭平 on 2023/09/13.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
